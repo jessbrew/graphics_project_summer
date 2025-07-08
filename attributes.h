294d37f8de2ba7b3dcfa42b33647287a1f8c96b5
Drew W <droomail23@gmail.com>
2025-07-08 11:25:20 -0500

    Added to attributes.h

--- attributes.h
+++ attributes.h
@@ -3,9 +3,13 @@
 //
 
 #pragma once
+#include <string>
 
 class Attributes {
-
+public:
+    std::string stroke_color{"black"};
+    int stroke_width{3};
+    std::string fill_color{"black"};
 };
 
 
