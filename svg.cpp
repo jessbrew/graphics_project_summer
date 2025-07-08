30557c6edb6b7e0759f7469e6f83ceb84755c180
rickyhillzero <rickyhillzero@gmail.com>
2025-07-08 12:10:05 -0500

    update to svg constructor

--- svg.cpp
+++ svg.cpp
@@ -1,5 +1,18 @@
-//
-// Created by Ricky on 7/8/25.
-//
-
 #include "svg.h"
+#include <stdexcept>
+
+// Ricky
+Svg::Svg(int canvas_vertical_length, int canvas_horizontal_length)
+    :filename{filename}, canvas_vertical_length{canvas_vertical_length}, canvas_horizontal_length{canvas_horizontal_length} {
+    if (canvas_horizontal_length < 1) {
+        throw std::runtime_error("A positive horizontal canvas length is required.");
+    } else if (canvas_vertical_length < 1) {
+        throw std::runtime_error("A positive horizontal canvas length is required.");
+    }
+
+    // add file header
+    svg_lines.push_back("<html>");
+    svg_lines.push_back("<body>");
+    svg_lines.push_back("<svg width="" + std::to_string(Svg::canvas_horizontal_length) + '" height="''" xmlns="http://www.w3.org/2000/svg">');
+}
+
--- svg.h
+++ svg.h
@@ -1,10 +1,21 @@
 #pragma once
+#include <vector>
 #include "canvas.h"
 
+// Ricky
 class Svg : public Canvas{
 public:
-    Svg ();
+    Svg (std::string filename, int canvas_vertical_length, int canvas_horizontal_length);
+
     void draw_ellipse(Point radius, Point center, Attributes a) override;
     void draw_rectangle(int height, int width, Point start, Attributes a) override;
     void draw_line(Point start, Point end, Attributes a) override;
+
+    void output_svg_file();
+
+private:
+    int canvas_vertical_length;
+    int canvas_horizontal_length;
+    std::vector<std::string> svg_lines;
+    std::string filename;
 };
