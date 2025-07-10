42cdcf216ae5d6c81fc7db5211520076f03baea0
jbrew <jmbrewster605@gmail.com>
2025-07-10 13:41:25 -0500

    Finished polygons

--- polygon.cpp
+++ polygon.cpp
@@ -1,5 +1,29 @@
-//
-// Created by zhimo on 7/10/2025.
-//
 
 #include "polygon.h"
+
+Polygon::Polygon(const std::vector<Point> &points)
+    : points{points} {
+    if (points.size() < 3 ) {
+        throw std::runtime_error("Must have at least three points in a polygon");
+    }
+}
+
+Polygon::Polygon(const std::vector<Point> &points, int stroke_width, const std::string &stroke_color, const std::string &fill_color)
+    : Polygon(points){
+    attributes.stroke_width = stroke_width;
+    attributes.stroke_color = stroke_color;
+    attributes.fill_color = fill_color;
+}
+
+void Polygon::draw(Canvas &c) {
+    c.draw_polygon(points, attributes);
+}
+
+void Polygon::move(int x_move, int y_move) {
+    for (Point& p : points) {
+        p.x += x_move;
+        p.y += y_move;
+    }
+}
+
+
--- polygon.h
+++ polygon.h
@@ -1,16 +1,16 @@
-//
-// Created by zhimo on 7/10/2025.
-//
+#pragma once
 
-#ifndef POLYGON_H
-#define POLYGON_H
+#include <vector>
+#include "shape.h"
+#include "point.h"
 
+class Polygon : public Shape{
+public:
+    Polygon(const std::vector<Point>& points);
+    Polygon(const std::vector<Point>& points, int stroke_width, const std::string& stroke_color, const std::string& fill_color);
+    void draw(Canvas &c) override;
+    void move(int x_move, int y_move) override;
 
-
-class polygon {
-
+private:
+    std::vector<Point> points;
 };
-
-
-
-#endif //POLYGON_H
